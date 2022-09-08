461f366ecca492400e9b12e37f5cbe26810609fc
Elliot <walker.elliot@elev.ga.ntig.se>
2022-09-08 14:09:39 +0200

    prog5

---
+++ Gymnasiearbete1/Assets/MovePlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+   
+    void Update()
+    {
+       float xDirection = Input.GetAxis("Horizontal");
+       float zDirection = Input.GetAxis("Vertical");
+
+       Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+       transform.position += moveDirection;
+    }
+
+}
