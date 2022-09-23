8f75bd3ec4638f101089e8f999afef1f98deda39
Elliot <walker.elliot@elev.ga.ntig.se>
2022-09-23 10:36:38 +0200

    prog9

---
+++ Gymnasiearbete1/Assets/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+[Header("Movement")]
+public float moveSpeed;
+
+public Transform orientation;
+
+float horizontalInput;
+float verticalInput;
+
+Vector3 moveDirection;
+
+Rigidbody rb;
+
+private void Start()
+{
+    rb = GetComponent<Rigidbody>();
+    rb.freezeRotation = true;
+}
+
+private void Update()
+{
+    MyInput();
+}
+
+private void FixedUpdate() {
+    MovePlayer();
+}
+
+private void MyInput()
+{
+    horizontalInput = Input.GetAxisRaw("Horizontal");
+    verticalInput = Input.GetAxisRaw("Vertical");
+
+}
+
+private void MovePlayer()
+{
+    moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+    rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+}
+
+}
+
+    // public float speed = .5f;
+
+    // void Update()
+    // {
+    //     float xDirection = Input.GetAxis("Horizontal");
+    //     float zDirection = Input.GetAxis("Vertical");
+
+    //     Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+    //     transform.position += moveDirection * speed;
+    // }
+
